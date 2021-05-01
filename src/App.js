0db118b0576895b7ac748a59adde299c040b9819
jan9 <edujuhye@gmail.com>
2021-05-01 14:40:16 -0700

    Home: add Detail component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
+    <div className='App'>
+      <Router>
+        <Header />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/detail'>
+            <Detail />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
